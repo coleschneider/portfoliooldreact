cc3b5c37f726530b93e843b4953f0c7ac3eaf8dc
Cole Schneider <colekschneider@gmail.com>
2019-10-25 23:40:30 -0700

    eslint

--- src/hooks/useLazyImage.tsx
+++ src/hooks/useLazyImage.tsx
@@ -1,22 +1,22 @@
-import * as React from 'react';
+import * as React from 'react'
 
 const resolveImage = (url: string): Promise<void> => {
-    return new Promise((resolve, reject) => {
-      const image = new Image()
-      image.onload = () => resolve()
-      image.onerror = reject
-      image.src = url
-    })
-  }
+  return new Promise((resolve, reject) => {
+    const image = new Image()
+    image.onload = () => resolve()
+    image.onerror = reject
+    image.src = url
+  })
+}
 
-function useLazyImage(imageSrc: string, fallback: string){
-    const [src, setSource] = React.useState(fallback)
-    React.useEffect(() => {
-        resolveImage(imageSrc).then(() => {
-            setSource(imageSrc)
-        })
-    }, [])
-    return src
+function useLazyImage(imageSrc: string, fallback: string) {
+  const [src, setSource] = React.useState(fallback)
+  React.useEffect(() => {
+    resolveImage(imageSrc).then(() => {
+      setSource(imageSrc)
+    })
+  }, [imageSrc])
+  return src
 }
 
 export default useLazyImage
