a9bcca6c26feadde58b4d89c4822073816b43aa7
Cole Schneider <colekschneider@gmail.com>
2019-11-19 09:26:38 -0800

    changes

--- src/components/Card/__mocks__/cardsConfig.tsx
+++ src/components/Card/__mocks__/cardsConfig.tsx
@@ -1,17 +1,17 @@
-
-export const cardsById = { '1':
-   { 
-        id: 1,
-        description: ['A ', 'Test ', 'Image'],
-        cardImage: 'test-image.jpg',
-        placeholder: 'test-image-placeholder.jpg' 
-    },
-}
-
-export const cardsConfig =    [{ 
+export const cardsById = {
+  '1': {
     id: 1,
     description: ['A ', 'Test ', 'Image'],
     cardImage: 'test-image.jpg',
-    placeholder: 'test-image-placeholder.jpg' 
-} ]
+    placeholder: 'test-image-placeholder.jpg',
+  },
+}
 
+export const cardsConfig = [
+  {
+    id: 1,
+    description: ['A ', 'Test ', 'Image'],
+    cardImage: 'test-image.jpg',
+    placeholder: 'test-image-placeholder.jpg',
+  },
+]
