1feb86431017941d3b7e1da656cc2d03b640deee
Cole Schneider <colekschneider@gmail.com>
2019-12-01 23:13:03 -0800

    make dialog box look nicer

--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -61,7 +61,7 @@ const Container = styled.div<{ children: React.ReactNode; css: FlattenSimpleInte
   box-shadow: rgba(67, 90, 111, 0.3) 0px 0px 1px, rgba(67, 90, 111, 0.47) 0px 16px 24px -8px;
   z-index: 3;
   width: 80%;
-  ${media.xs`
+  ${media.sm`
     width: 392px;
   `}
 `
